38dd7855758c23b53581e281a621aef84a974097
alina-todea <137332096+alina-todea@users.noreply.github.com>
2023-07-09 10:42:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lab3ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Screiti un program care va numara vocalele dintr-un sir de caractere citit de la tastatura.
+
+            string vocale = "aeiou";
+            Console.WriteLine("Introduceti un sir de caractere");
+            string mesaj = Console.ReadLine();
+
+            int length = mesaj.Length;
+            int lengthVocale = vocale.Length;
+
+            int count = 0;
+
+            for (int i=0; i<length; i++)
+
+            {
+
+                for (int j=0; j<lengthVocale; j++)
+
+                {
+
+                if ( Char.ToLower(vocale[j]) == Char.ToLower( mesaj[i]) )
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Numarul total de vocale este: {0} ", count);
+
+
+
+        }
+    }
+}
